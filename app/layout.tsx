5e81e472ddf6571766efdff1cb21b3d3f34ce1ed
guschi18 <philipp.guschl@gmx.de>
2023-12-22 08:55:51 +0100

    title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'ThirdWeb-Login',
+  title: 'ThirdWeb Web3-Login',
   description: 'Example ThirdWeb Login',
 }
 
